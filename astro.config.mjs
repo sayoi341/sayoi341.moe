3642d9b0fffcb902b326ec276c0ac141b05028fa
sayoi341 <me@sayoi341.moe>
2023-12-05 11:51:16 +0000

    🚧 cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  output: "server",
+  adapter: cloudflare()
 });
